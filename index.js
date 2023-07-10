ae7031682254006fd752e04142ff16fbd2b737ed
周培军 <zhoupeijun@greatwall.com.cn>
2023-07-10 23:29:51 +0800

    修改打包配置

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow ,globalShortcut} = require("electron");
 const path = require("path");
 
 const server = require("./server/index");
@@ -7,18 +7,22 @@ function createWindow(port) {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false,
+    frame: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   });
 
   win.loadURL(`http://127.0.0.1:${port}`);
+  win.setMenu(null);
+  globalShortcut.register('F11',()=>{
+    win.webContents.openDevTools();
+  })
   // win.loadFile(`index.html`);
-  win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
+
   server.start(8000).then((port) => {
     console.log("服务开启端口:", port);
     createWindow(port);
